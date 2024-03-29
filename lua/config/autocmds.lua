ecd49b5c5eeb99ee166163b59e84071cf455788d
Darrell-Devana <darrell.putra@binus.ac.id>
2024-03-29 13:05:53 +0700

    first commit

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable autoformat for C/C++ files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "cpp", "c" },
+    callback = function()
+        vim.b.autoformat = false
+    end,
+})
+
+-- Disable autoformat for python files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "py" },
+    callback = function()
+        vim.b.autoformat = false
+    end,
+})
